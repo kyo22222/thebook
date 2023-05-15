ef965bd3a416bae28da350685f8bdac484855619
Dino.C <40124790+kyo22222@users.noreply.github.com>
2023-05-15 15:40:50 +0800

    content update

--- src/app/app.resume.ts
+++ src/app/app.resume.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'resume',
@@ -7,15 +7,26 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
 })
 
 export class resume_compo{
+
   toggleRESU(){
     this.resume_tog = !this.resume_tog;//toggle
     this.togAct.emit(this.resume_tog);
   }
 
+  ngOnChanges(resume_tog:any) {
+    for (let propName in resume_tog) {
+      let chng = resume_tog[propName];
+      let cur  = JSON.stringify(chng.currentValue);
+      let prev = JSON.stringify(chng.previousValue);
+      console.log(cur, prev);
+    }
+  }
+
   //output
   @Output() togAct = new EventEmitter(); 
   constructor() { }
-  ngOnInit() {}
+  ngOnInit() {
+  }  
 
   @Input()
   //myName!: string;
